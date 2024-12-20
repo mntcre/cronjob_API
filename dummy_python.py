a642de3b73a0cf9edc15a3a8102e8a6ad7987655
mntcre <maeum@gmx.de>
2024-12-20 23:16:45 +0100

    changes in directory call to make independent of bash automatization

--- dummy_python.py
+++ dummy_python.py
@@ -6,23 +6,26 @@ Created on Tue Nov 12 08:39:19 2024
 """
 
 from datetime import datetime
+import os
+import sys
 
-# import os
-# from bs4 import BeautifulSoup
 
 # Getting the current date and time
 dt = datetime.now()
 date = str(dt.date())
 time = str(dt.time())
 
-filepath = __file__[:__file__.rfind('\\')+1]
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
+# Change the working directory to the script's directory
+#os.chdir(script_dir)
+print(script_dir)
 
 logtext = f'''Log-{dt} \n
 Test Output \n 
 ----------------------------------\n'''
 
-with open(filepath + "logfile.txt", "a") as output:
+with open(script_dir + "\\logfile.txt", "a") as output:
     output.write(logtext)
 
 
